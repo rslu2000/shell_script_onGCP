c28e8dc614c325115651d58f64ba3e01f4e33381
盧瑞山 <admin@luruishande-MacBook-Pro.local>
2017-09-01 13:59:41 +0800

    add files

---
+++ nodejs_install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install gcc g++ -y
+sudo apt-get install make -y
+wget https://nodejs.org/dist/v8.1.2/node-v8.1.2.tar.gz 
+tar xvf node-v8.1.2.tar.gz
+cd node-v8.1.2
+./configure
+make -j4
+sudo make install
--- replace.sh
+++ replace.sh
@@ -3,19 +3,19 @@ sudo find ./ -type f -exec sed -i "s/bitcoin/abccoin/g" {} \;
 sudo find ./ -type f -exec sed -i "s/bitcoin/abccoin/g" {} \; 
 sudo find ./ -type f -exec sed -i "s/Bitcoin/Abccoin/g" {} \;
 sudo find ./ -type f -exec sed -i "s/BTC/ABC/g" {} \;
-for file in find ./
+for file in `find ./`
         do
-                newname=echo $file | sed "s/bitcoin/abccoin/g";
+                newname=`echo $file | sed "s/bitcoin/abccoin/g"`
                 mv $file $newname;
         done
-for file in find ./
+for file in `find ./`
         do
-                newname=echo $file | sed "s/Bitcoin/Abccoin/g";
+                newname=`echo $file | sed "s/Bitcoin/Abccoin/g"`
                 mv $file $newname;
         done
-for file in find ./
+for file in `find ./`
         do
-                newname=echo $file | sed "s/BTC/ABC/g";
+                newname=`echo $file | sed "s/BTC/ABC/g"`
                 mv $file $newname;
         done
 
