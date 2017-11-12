87b99a5f849d3e2c76c82748ee2ef1e6bdfaa0ec
tmust <rslu3000@gmail.com>
2017-11-13 06:49:09 +0800

    fixed

--- x11vnc_novnc_install.sh
+++ x11vnc_novnc_install.sh
@@ -16,7 +16,7 @@ sudo x11vnc --storepasswd /opt/x11vnc.pwd
 sudo x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /opt/x11vnc.pwd -rfbport 5900 -shared -display :1 &
 git clone https://github.com/kanaka/noVNC
 cd noVNC
-./utils/launch.sh --vnc localhost:5900 —listen 6080
+nohup ./utils/launch.sh --vnc localhost:5900 --listen 6080 &
 
 ETIME=`date`
 echo $STIME,$ETIME 
