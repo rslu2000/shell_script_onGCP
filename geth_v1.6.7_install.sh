b7b21dca0ac44af7a96293dafd3ae4d0c866dbdc
rslu2000 <rslu2000@gmail.com>
2017-11-29 10:05:13 +0800

    fixed

--- geth_v1.6.7_install.sh
+++ geth_v1.6.7_install.sh
@@ -1,8 +1,11 @@
 curl -O https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.7.1.linux-amd64.tar.gz
 mkdir -p ~/go; echo "export GOPATH=$HOME/go" >> ~/.bashrc
+rm -rf go1.7.1.linux-amd64.tar.gz
 sudo cp /usr/local/go/bin/go /usr/bin/
 git clone -b v1.6.7 https://github.com/ethereum/go-ethereum
 cd go-ethereum
 make all
-sudo cp ./build/bin/* /usr/bin/.
+sudo cp ./build/bin/* /usr/bin/
+cd ..
+rm -rf go-ethereum
